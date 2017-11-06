e9c09d7a48b85aa89b590b35c6ca6a11ce29bc7e
David Christensen <david@endpoint.com>
2017-11-06 09:49:57 -0600

    Move axes into code instead of hard-coded expected filenames and paths

--- test_regex_overhead.pl
+++ test_regex_overhead.pl
@@ -4,48 +4,79 @@ use strict;
 use warnings;
 use autodie;
 
+require 5.010;                       # needed for *FAIL regex verb
+
+use Data::Dumper;
+
 # latest versions in each major branch
 my @default_versions = qw/5.8.9 5.10.1 5.12.5 5.14.4 5.16.3 5.18.4 5.20.3 5.22.3 5.24.1 5.26.1/;
 my @target_versions = @ARGV ? @ARGV : @default_versions;
 
+my @axes = qw/Encode Safe UTF8/;
+my %script_re = get_script_re();
+
 install_missing_versions();
 setup_scripts();
+check_scripts();
+print "Everything generated; sleeping 5 so you can cancel...\n";
+sleep 5;
 run_tests();
 
 
 #################### Routines ####################
 
+sub get_script_re {
+    # return permutations/regexes for all combinations of the axes
+    # using the fact that bitwise operations encode all perms for the powers of two
+
+    my %regs;
+    my %col_offset;
+    @col_offset{@axes} = (0..@axes-1);
+    for my $perm (0..2**@axes-1) {
+        my @re;
+        my @fn;
+        for my $axis (@axes) {
+            my $include = vec $perm, $col_offset{$axis}, 1;
+            push @re, $axis if $include;
+            push @fn, ($include ? 'no' : '') . lc $axis;
+        }
+        my $re = @re ? join '|', @re : '(*FAIL)';
+        my $fn = (join '_' => ('with', @fn)) . '.pl';
+        $regs{$fn} = qr/$re/;
+    }
+    return %regs;
+}
+
 sub run_tests {
+    my @fns = sort keys %script_re;
     for my $vers (@target_versions) {
-        print "Version: $vers (Encode, No Safe)\n";
-        system("PLENV_VERSION=$vers time plenv exec perl with_encode_without_safe.pl");
-        print "Version: $vers (Encode, Safe)\n";
-        system("PLENV_VERSION=$vers time plenv exec perl with_encode_with_safe.pl");
-        print "Version: $vers (No Encode, No Safe)\n";
-        system("PLENV_VERSION=$vers time plenv exec perl without_encode_without_safe.pl");
-        print "Version: $vers (No Encode, Safe)\n";
-        system("PLENV_VERSION=$vers time plenv exec perl without_encode_with_safe.pl");
+        for my $fn (@fns) {
+            print "Version: $vers ($fn)\n";
+            system("PLENV_VERSION=$vers time plenv exec perl $fn");
+        }
     }
 }
 
 sub setup_scripts {
     my @prog = <DATA>;
 
-    open my $enc_safe, '>', 'with_encode_with_safe.pl';
-    open my $enc_nosafe, '>', 'with_encode_without_safe.pl';
-    open my $noenc_safe, '>', 'without_encode_with_safe.pl';
-    open my $noenc_nosafe, '>', 'without_encode_without_safe.pl';
+    my @fns = sort keys %script_re;
+    my %fh;
+    
+    open $fh{$_}, '>', $_ for @fns;
 
     for (@prog) {
-        print $enc_safe $_;
-        print $enc_nosafe $_ unless /Safe/;
-        print $noenc_safe $_ unless /Encode/;
-        print $noenc_nosafe $_ unless /Encode/ || /Safe/;
+        for my $fn (@fns) {
+            $fh{$fn}->print($_) unless /$script_re{$fn}/;
+        }
     }
-    close $enc_safe;
-    close $enc_nosafe;
-    close $noenc_safe;
-    close $noenc_nosafe;
+    close $fh{$_} for @fns;
+}
+
+sub check_scripts {
+    my @fns = sort keys %script_re;
+
+    system("perl -cw $_") for @fns;
 }
 
 sub install_missing_versions {
@@ -68,6 +99,7 @@ __DATA__
 use strict;
 use warnings;
 
+use utf8;                       # UTF8
 use Encode;
 use Safe;
 
